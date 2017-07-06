7ab8a35a1eab7f2f3642b863985a237226aa25d1
DESKTOP-8E79NQI\Karen <kareen2707@gmail.com>
2017-07-06 17:23:36 +0200

    Finite State Machine files

---
+++ fsm.c
@@ -0,0 +1,47 @@
+/*  ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+ B105- Electronic Systems Lab
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "fsm.h"
+
+fsm_t*
+fsm_new (fsm_trans_t* tt)
+{
+  fsm_t* this = (fsm_t*) malloc (sizeof (fsm_t));
+  fsm_init (this, tt);
+  return this;
+}
+
+void
+fsm_init (fsm_t* this, fsm_trans_t* tt)
+{
+  this->tt = tt;
+}
+
+void
+fsm_fire (fsm_t* this)
+{
+  fsm_trans_t* t;
+  for (t = this->tt; t->orig_state >= 0; ++t) {
+    if ((this->current_state == t->orig_state) && t->in(this)) {
+      this->current_state = t->dest_state;
+      if (t->out)
+        t->out(this);
+      break;
+    }
+  }
+}
